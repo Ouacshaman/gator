bdbb790943492b9fcda7423db832da566cca0324
Shi Hong <shaquanhong@gmail.com>
2024-11-07 22:56:29 -0500

    I: createfeedfollow query CTE

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -35,3 +35,36 @@ SELECT feeds.name, feeds.url, users.name
 FROM feeds
 LEFT JOIN users
 ON feeds.user_id = users.id;
+
+-- name: CreateFeedFollow :many
+WITH inserted_feed_follow AS (
+    INSERT INTO feed_follows (id, created_at, updated_at, feed_id, user_id)
+    VALUES(
+        $1,
+        $2,
+        $3,
+        $4,
+        $5
+    )
+    RETURNING *
+)
+SELECT
+    inserted_feed_follow.*,
+    feeds.name AS feed_name,
+    users.name AS user_name
+FROM inserted_feed_follow
+INNER JOIN users ON inserted_feed_follow.user_id = users.id
+INNER JOIN feeds ON inserted_feed_follow.feed_id = feeds.id;
+
+
+
+
+
+
+
+
+
+
+
+
+
