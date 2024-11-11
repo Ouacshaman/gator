a1cf04ef341140682bba62a0883334362bf380b6
Shi Hong <shaquanhong@gmail.com>
2024-11-10 21:10:10 -0500

    L: Getfeedfollowuser query

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -61,7 +61,13 @@ SELECT *
 FROM feeds
 WHERE feeds.url = $1;
 
-
+-- name: GetFeedFollowsForUser :many
+SELECT feed_follows.*, users.name AS user_name, feeds.name AS feed_name FROM feed_follows
+INNER JOIN users
+ON feed_follows.user_id = users.id
+INNER JOIN feeds
+ON feed_follows.feed_id = feeds.id
+WHERE feed_follows.user_id = $1;
 
 
 
