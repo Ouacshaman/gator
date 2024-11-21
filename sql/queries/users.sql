e6dfe0db25f1e0a0e1ca0fd343b5ddcf40f79c71
Shi Hong <shaquanhong@gmail.com>
2024-11-20 20:01:47 -0500

    T: getpostuser

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -83,7 +83,22 @@ WHERE id = $1;
 SELECT * FROM feeds
 ORDER BY last_fetched_at NULLS FIRST LIMIT 1;
 
+-- name: CreatePost :one
+INSERT INTO posts (
+    id, created_at, updated_at, title, url, description, published_at, feed_id
+) VALUES ( $1, $2, $3, $4, $5, $6, $7, $8)
+RETURNING *;
 
+-- name: GetPost :one
+SELECT * FROM posts
+WHERE posts.url = $1;
+
+-- name GetPostsUser :many
+SELECT posts.* FROM posts
+INNER JOIN feed_follows AS follows
+ON feed_id = follows.feed_id
+WHERE follows.user_id = $1
+ORDER BY published_at NULLS FIRST DESC, created_at DESC LIMIT $2;
 
 
 
